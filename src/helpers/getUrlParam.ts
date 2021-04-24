1754f8f252e7d55aa83e86875d9e9f414284641a
HeitorRamos132 <heitorgpramos95@gmail.com>
2021-04-24 14:43:54 -0300

    created method to get url params

---
+++ src/helpers/getUrlParam.ts
@@ -0,0 +1,7 @@
+const getUrlParam = (param: string): string | null => {
+    const parameters = new URLSearchParams(window.location.search);
+    const parameter = parameters.get(param);
+    return parameter
+}
+
+export default getUrlParam
