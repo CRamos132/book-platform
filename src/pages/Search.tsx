cecbc41f1ede247fc091f184fbad9321269be575
HeitorRamos132 <heitorgpramos95@gmail.com>
2021-04-24 17:06:14 -0300

    added search results to page

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,20 +1,47 @@
 import React, {useEffect, useState} from 'react'
 import Input from '../components/atoms/Input/Input';
+import BookList from '../components/molecules/BookList/BookList';
 import NavMenu from '../components/molecules/NavMenu/NavMenu';
 import Main from '../components/templates/Main/Main';
+import useDebounce from '../helpers/debounce';
+import getBooks from '../helpers/getBooks';
 import getUrlParam from '../helpers/getUrlParam';
 
 const Search: React.FC = () => {
-    const [query, setQuery] = useState('')
+    const [displayQuery, setDisplayQuery] = useState('')
+    const [books, setBooks] = useState<any>([])
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const {value} = event.target
+        setDisplayQuery(value)
+        debouncedAction(value)
+    }
+    const handleQuery = (search: string) => {
+        if(!search){
+            return
+        }
+        setBooks([])
+        getBooks(search).then(async response => {
+            const data = await response.json()
+            console.log(data)
+            setBooks(data.items)
+        })
+    }
+    const debouncedAction = useDebounce(handleQuery, 500)
     useEffect(()=>{
         const newQuery = getUrlParam('q')
-        setQuery(newQuery || '')
+        const search = newQuery || ''
+        setDisplayQuery(search)
+        getBooks(search).then(async response => {
+            const data = await response.json()
+            console.log(data)
+            setBooks(data.items)
+        })
     }, [])
     return (
         <Main 
-        top={<Input type='search' name='quey' placeholder='Search' />}
-        body={<div style={{height: '50px', width: '300px', display: 'inline-block'}}>{query}</div>}
-        bottom={<NavMenu active='home' />}
+            top={<Input type='search' name='quey' value={displayQuery} onChange={handleChange} placeholder='Search' />}
+            body={<BookList books={books} />}
+            bottom={<NavMenu active='home' />}
         />
     )
 }
