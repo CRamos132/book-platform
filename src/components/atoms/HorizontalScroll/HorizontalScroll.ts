a2b5a6e74b6b151ef32a13ff9d5514dcae262554
HeitorRamos132 <heitorgpramos95@gmail.com>
2021-04-24 14:44:37 -0300

    created horizontal 'carousel' display

---
+++ src/components/atoms/HorizontalScroll/HorizontalScroll.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const HorizontalScroll = styled.article`
+    display: block;
+    min-height: 100px;
+    padding-left: 20px;
+    overflow-x: scroll;
+    overflow-y: hidden;
+    white-space: nowrap;
+`
+
+export default HorizontalScroll
