80865103e45a6087d010441e67d752848ba61205
HeitorRamos132 <heitorgpramos95@gmail.com>
2021-04-24 14:44:11 -0300

    created main input

---
+++ src/components/atoms/Input/Input.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+const Input = styled.input`
+    background-color: ${props => props.theme.white};
+    border-radius: 10px;
+    font-size: 16px;
+    color: black;
+    padding: 15px;
+    width: 90%;
+    margin: auto;
+    border: none;
+    ::placeholder,
+    ::-webkit-input-placeholder {
+        color: ${props => props.theme.gray};
+    }
+    :-ms-input-placeholder {
+        color: ${props => props.theme.gray};
+    }
+`
+
+export default Input
