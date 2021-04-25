f24cfe3faaa5161e17bb339b7a5b6ded71880fdf
HeitorRamos132 <heitorgpramos95@gmail.com>
2021-04-25 15:55:59 -0300

    added new params to theme

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -1,7 +1,9 @@
 const theme  = {
     bgColor: '#fff6e5',
+    lightBg: '#fffdf9',
     white: '#FDFCFC',
     gray: '#54565A',
+    shadow: '3px 6px 23px rgba(107, 103, 70, 0.125901)'
 }
 
 export default theme
