2929977ea6afea31459fdfac5ef11625f7e8a8a4
HeitorRamos132 <heitorgpramos95@gmail.com>
2021-04-25 15:56:38 -0300

    created bigger book cover component

---
+++ src/components/atoms/BookCoverMd/BookCoverMd.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const BookCoverMd = styled.img`
+    height: 234px;
+    width: auto;
+    box-shadow: ${props => props.theme.shadow};
+`
+
+export default BookCoverMd
