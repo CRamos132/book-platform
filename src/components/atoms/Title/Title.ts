02264da33267d4937a01fb53e02b2bb3d00fb6d1
HeitorRamos132 <heitorgpramos95@gmail.com>
2021-04-25 15:57:18 -0300

    created typography components

---
+++ src/components/atoms/Title/Title.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Title = styled.span`
+    font-size: 0.7em;
+    margin: 10px 0px 5px;
+    width: 100%;
+    text-align: left;
+    font-weight: bold;
+`
+
+export default Title
