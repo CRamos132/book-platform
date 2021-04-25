3d369a10bcc61ed3d5bb4158ff143cdabb92d323
HeitorRamos132 <heitorgpramos95@gmail.com>
2021-04-25 15:57:08 -0300

    created card wrapper component

---
+++ src/components/atoms/CardWrapper/CardWrapper.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const CardWrapper = styled.article`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    max-width: 30%;
+    color: rgba(49, 49, 49, 0.8);
+`
+
+export default CardWrapper
