7f1567aa25cab4f81923e3dc043a98f86fa95e22
HeitorRamos132 <heitorgpramos95@gmail.com>
2021-04-26 10:36:51 -0300

    created back button component

---
+++ src/components/atoms/BackButton/BackButton.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+import {TransparentButton} from './style'
+
+interface BackButtonProps {
+    onClick?: ()=>void;
+}
+
+const BackButton: React.FC<BackButtonProps> = ({onClick}) => {
+    const history = useHistory();
+    const handleClick = () => {
+        if(onClick){
+            onClick()
+        } else {
+            history.goBack()
+        }
+    }
+
+    return (
+        <TransparentButton onClick={handleClick}>
+            <img src='./images/back.svg' alt='Arrow left' />
+        </TransparentButton>
+    )
+}
+
+export default BackButton
---
+++ src/components/atoms/BackButton/style.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const TransparentButton = styled.button`
+    position: absolute;
+    top: 55px;
+    left: 33px;
+    background-color: transparent;
+    border: none;
+    img {
+        height: 14px;
+        width: 14px;
+    }
+`
+
+export {TransparentButton}
