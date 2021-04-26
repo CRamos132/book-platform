e8fd6d8cdf6b10c6c01aa6b849047f0f684b1a9d
HeitorRamos132 <heitorgpramos95@gmail.com>
2021-04-26 17:06:21 -0300

    updated spacing

--- src/components/atoms/TextContainer/TextContainer.ts
+++ src/components/atoms/TextContainer/TextContainer.ts
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 const TextContainer = styled.article`
     width: 90%;
     margin: auto;
-    margin-bottom: 150px;
     display: flex;
     flex-direction: column;
     align-items: center;
