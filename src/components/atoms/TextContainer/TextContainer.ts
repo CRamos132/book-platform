add8470488e91a8112c6c3cc36459a334057712f
HeitorRamos132 <heitorgpramos95@gmail.com>
2021-04-26 10:18:42 -0300

    added text container component

---
+++ src/components/atoms/TextContainer/TextContainer.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const TextContainer = styled.article`
+    width: 90%;
+    margin: auto;
+    margin-bottom: 30px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+export default TextContainer
