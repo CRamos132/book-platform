2ec9a7efcfc74d7fa908cfdffad0d84def42cd2f
HeitorRamos132 <heitorgpramos95@gmail.com>
2021-04-26 12:11:14 -0300

    updated bottom margin to be above floating menu

--- src/components/atoms/TextContainer/TextContainer.ts
+++ src/components/atoms/TextContainer/TextContainer.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 const TextContainer = styled.article`
     width: 90%;
     margin: auto;
-    margin-bottom: 60px;
+    margin-bottom: 150px;
     display: flex;
     flex-direction: column;
     align-items: center;
