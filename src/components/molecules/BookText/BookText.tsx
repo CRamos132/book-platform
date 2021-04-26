785a9193cafea272b759601d35abf6d640c7c766
HeitorRamos132 <heitorgpramos95@gmail.com>
2021-04-26 10:18:52 -0300

    created book text component

---
+++ src/components/molecules/BookText/BookText.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Header from '../../atoms/Header/Header'
+import SubHeader from '../../atoms/SubHeader/SubHeader'
+import TextBody from '../../atoms/TextBody/TextBody'
+import TextContainer from '../../atoms/TextContainer/TextContainer'
+
+interface BookTextProps {
+    title: string;
+    subtitle?: string;
+    authors: string[];
+    description: string;
+}
+
+const BookText: React.FC<BookTextProps> = ({title, subtitle, authors, description}) => {
+    return (
+        <TextContainer>
+            <Header>
+                {title}
+                {subtitle && (
+                    <span className='sub'>: {subtitle}</span>
+                )}
+            </Header>
+            <SubHeader>
+                {authors?.map((author, index) => 
+                    index === authors.length - 1 ? `${author}` : `${author}, `
+                )}
+            </SubHeader>
+            <TextBody>
+                {description}
+            </TextBody>
+        </TextContainer>
+    )
+}
+
+export default BookText
