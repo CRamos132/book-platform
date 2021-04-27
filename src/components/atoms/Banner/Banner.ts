0eb9c0eb05b8c1d746c63f883fed5d104111d343
HeitorRamos132 <heitorgpramos95@gmail.com>
2021-04-27 15:31:43 -0300

    updated book card width

--- src/components/atoms/Banner/Banner.ts
+++ src/components/atoms/Banner/Banner.ts
@@ -7,19 +7,30 @@ interface BannerProps {
 const Banner = styled.article<BannerProps>`
     display: inline-block;
     position: relative;
-    width: 272px;
+    min-width: 272px;
     height: 139px;
     background: ${(props) => (props.type === '1' ? '#00173D' : '#451475')};
     box-shadow: 2px 4px 18px rgba(154, 175, 209, 0.62134);
     border-radius: 5px;
     z-index: 1;
     .wrapper {
+        z-index: 1;
         width: 100%;
         height: 100%;
         padding: 15px 20px;
         display: flex;
         flex-direction: row;
         justify-content: space-between;
+    }
+    .text-content {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+        h4 {
+            max-width: 190px;
+            overflow: hidden;
+            text-overflow: ellipsis;
+        }
     }
     img {
         height: 111px;
