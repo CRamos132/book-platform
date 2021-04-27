61cd1ce45aa85de892a7497a55c2fcf6be287423
HeitorRamos132 <heitorgpramos95@gmail.com>
2021-04-27 09:50:23 -0300

    added svgs to currently reading component

--- src/components/organisms/CurrentCard/CurrentCard.tsx
+++ src/components/organisms/CurrentCard/CurrentCard.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReadingTypography from '../../atoms/ReadingTypography/ReadingTypography'
 import ReadingWrapper from '../../atoms/ReadingWrapper/ReadingWrapper'
 import styled from 'styled-components'
+import ReadNow from '../../atoms/ReadNow/ReadNow'
 
 const Background = styled.div`
     z-index: -1;
@@ -10,12 +11,46 @@ const Background = styled.div`
     background-color: #EEF5DB;
     height: 100px;
     width: 100%;
+    overflow: hidden;
+    .top-right {
+        position: absolute;
+        right: -20px;
+        top: -20px;
+    }
+    .top-mid {
+        position: absolute;
+        right: 90px;
+        top: -6px;
+        height: 18px;
+        width: 18px;
+    }
+    .bottom-right {
+        position: absolute;
+        right: -23px;
+        bottom: 15px;
+    }
 `
 
 const CurrentCard: React.FC = () => {
     return (
         <ReadingWrapper>
-            <Background />
+            <Background>
+                <img 
+                    className='top-right' 
+                    src='./images/linedCircle.svg' 
+                    alt='Background circle' 
+                />
+                <img 
+                    className='top-mid' 
+                    src='./images/emptyCircle.svg' 
+                    alt='Background circle' 
+                />
+                <img 
+                    className='bottom-right' 
+                    src='./images/pinkLine.svg' 
+                    alt='Background circle' 
+                />
+            </Background>
             <img 
                 className='book-cover'
                 src='https://via.placeholder.com/100x153' 
@@ -25,6 +60,19 @@ const CurrentCard: React.FC = () => {
                 <div>
                     <ReadingTypography text='Originals' />
                     <ReadingTypography text='by Adam Grant' subHeader={true}/>
+                </div>
+                <div className="align-bottom">
+                    <ReadNow src='./images/bookmark.svg' regularColor='black' highlightColor='#FF6978'>
+                        <span>
+                            Chapter
+                        </span>
+                        <strong>
+                            2 
+                        </strong>
+                        <span>
+                            From 9
+                        </span>
+                    </ReadNow>
                 </div>
             </div>
         </ReadingWrapper>
