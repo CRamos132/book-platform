edaca6dcbe9b62380a30d86fa2913e382748b275
HeitorRamos132 <heitorgpramos95@gmail.com>
2021-04-28 13:49:20 -0300

    updated text style

--- src/components/atoms/BannerTypography/style.ts
+++ src/components/atoms/BannerTypography/style.ts
@@ -1,12 +1,16 @@
 import styled from 'styled-components';
 
 const Header = styled.h3`
+    width: 100%;
+    text-align: left;
     font-family: PlayfairDisplay, serif;
     color: ${(props) => props.color};
     font-size: 1.5em;
 `;
 
 const SubHeader = styled.h4`
+    width: 100%;
+    text-align: left;
     color: ${(props) => props.color};
     margin-top: 5px;
     font-size: 0.8em;
