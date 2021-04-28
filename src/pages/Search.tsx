32b3c7cb4bef9a9ae4da79eac053208427babbcc
HeitorRamos132 <heitorgpramos95@gmail.com>
2021-04-28 13:31:13 -0300

    added function that was in different component

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -11,11 +11,18 @@ const Search: React.FC = () => {
   const [displayQuery, setDisplayQuery] = useState('');
   const [query, setQuery] = useState('');
   const [books, status, loadMore] = useQuery(query);
+  const [count, setCount] = useState(0);
   const debouncedAction = useDebounce(setQuery, 500);
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     const { value } = event.target;
     setDisplayQuery(value);
+    setCount(0);
     debouncedAction(value);
+  };
+  const handleLoad = (): void => {
+    const newCount = count + 12;
+    loadMore(String(newCount));
+    setCount(newCount);
   };
   useEffect(() => {
     const newQuery = getUrlParam('q');
@@ -34,7 +41,7 @@ const Search: React.FC = () => {
           placeholder="Search"
         />
 )}
-      body={<BookList books={books} status={status} loadMore={loadMore} />}
+      body={<BookList books={books} status={status} loadMore={handleLoad} />}
       bottom={<NavMenu active="home" />}
     />
   );
