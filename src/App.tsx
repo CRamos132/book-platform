6867fc9caa688556012a3fadd95ce3f737c25229
HeitorRamos132 <heitorgpramos95@gmail.com>
2021-04-28 14:08:37 -0300

    added not found page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import BookDetails from './pages/BookDetails';
 import Landing from './pages/Landing';
+import NotFound from './pages/NotFound';
 import Search from './pages/Search';
 
 const App: React.FC = () => (
@@ -10,11 +11,22 @@ const App: React.FC = () => (
       <Route path="/" exact>
         <Landing />
       </Route>
+      <Route path="/home">
+        <Landing />
+      </Route>
       <Route path="/search" exact>
         <Search />
       </Route>
       <Route path="/book" exact>
         <BookDetails />
+      </Route>
+      {/*
+        because we have multiple cases where the homepage
+        links don't exist I've taken the liberty to use the 404
+        route to handle those cases
+      */}
+      <Route>
+        <NotFound />
       </Route>
     </Switch>
   </BrowserRouter>
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import styled from 'styled-components';
+import Button from '../components/atoms/Button/Button';
+
+const Wrapper = styled.main`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    h1 {
+        margin: 10vh 0;
+        width: 100%;
+        text-align: center;
+    }
+    .btn-wrapper {
+        width: 90%;
+    }
+`;
+
+const NotFound: React.FC = () => {
+  const history = useHistory();
+  const handleBack = (previous?: boolean): void => {
+    if (previous) {
+      history.goBack();
+    } else {
+      history.push('/');
+    }
+  };
+  return (
+    <Wrapper>
+      <h1>Page under construction</h1>
+      <div className="btn-wrapper">
+        <Button onClick={() => { handleBack(); }}>Go to home</Button>
+        <Button onClick={() => { handleBack(true); }}>Previous page</Button>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default NotFound;
